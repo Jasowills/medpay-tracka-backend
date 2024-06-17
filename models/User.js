a968bbc533144661b785fa97d4c9ad2ece6f901e
Jasowills <Jujuzhaddy01@gmail.com>
2024-06-17 15:44:24 +0100

    origin

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true},
     password: { type: String },
     email: { type: String},
     firstName: { type: String },
