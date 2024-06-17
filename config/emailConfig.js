31a86b17d020525b98f58882c64bf0831aa334fb
Jasowills <Jujuzhaddy01@gmail.com>
2024-06-17 15:51:54 +0100

    orgin

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -1,11 +1,14 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
 const nodemailer = require('nodemailer');
 
 // Create a transporter object using SMTP transport
 const transporter = nodemailer.createTransport({
     service: 'Gmail', // Use your email service provider
     auth: {
-        user: 'medpaytrackateam@gmail.com', // Your email address
-        pass: 'lrzpqfnrweezfkjx', // Your email password or API key
+        user: process.env.EMAIL_USER, // Your email address from environment variable
+        pass: process.env.EMAIL_PASS, // Your email password or API key from environment variable
     },
 });
 
